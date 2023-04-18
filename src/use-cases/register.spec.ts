f78bb4a7df30386e6192ec2b860a8d239155c3c1
Gabriel Pires <gabpiresdev@gmail.com>
2023-04-18 18:48:20 -0300

    create a in memory repository to use in tests

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,23 +1,13 @@
 import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UsersAlreadyExistsError } from './errors/users-already-exists'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
-    const register = new RegisterUseCase({
-      async findByEmail() {
-        return null
-      },
-      async create(data) {
-        return {
-          id: '1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          createdAt: new Date(),
-        }
-      },
-    })
+    const usersRepository = new InMemoryUsersRepository()
+    const register = new RegisterUseCase(usersRepository)
 
     const { user } = await register.execute({
       name: 'John Doe',
@@ -31,5 +21,39 @@ describe('Register Use Case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const register = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await register.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      register.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UsersAlreadyExistsError)
+  })
+
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const register = new RegisterUseCase(usersRepository)
+
+    const { user } = await register.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
   })
 })
