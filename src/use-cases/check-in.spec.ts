ca0b4b2db10d5a1c3e4b1feb33d7a6c520197d5d
Gabriel Pires <gabpiresdev@gmail.com>
2023-05-02 19:07:52 -0300

    list user check ins history use case and tests

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -10,7 +10,7 @@ let checkInsRepository: InMemoryCheckInsRepository
 let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
-describe('Get User Profile Use Case', () => {
+describe('Check In Use Case', () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     gymsRepository = new InMemoryGymsRepository()
