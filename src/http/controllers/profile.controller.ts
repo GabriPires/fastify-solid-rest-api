ac11b8f912c517505f8170e89afc6348fc9c55ab
Gabriel Pires <gabpiresdev@gmail.com>
2023-05-04 20:17:43 -0300

    declare types for fastify jwt user object

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -3,7 +3,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export async function profile(req: FastifyRequest, res: FastifyReply) {
   await req.jwtVerify()
 
-  console.log(req.user)
+  console.log(req.user.sub)
 
   return res.status(200).send()
 }
